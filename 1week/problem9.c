944a9e5a688d65e10acedfeb08a1abdf92b338ba
ttae-young <happysally05@naver.com>
2024-09-27 23:14:18 +0900

    Solve 프로그래머스 문제 '나머지 구하기'

---
+++ 1week/problem9.c
@@ -0,0 +1,17 @@
+// # 나머지 구하기
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2)
+{
+    int answer = num1 % num2;
+    return answer;
+}
+
+int main()
+{
+    printf("asdasda");
+    return 0;
+}
