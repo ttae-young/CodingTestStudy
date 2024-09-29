55845355b9805f235b599bf3aeef9baff5c05692
ttae-young <happysally05@naver.com>
2024-09-29 18:11:15 +0900

    Solve 프로그래머스 문제 '배열 뒤집기'

---
+++ 1week/problem20.c
@@ -0,0 +1,16 @@
+// # 배열 뒤집기
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int num_list[], size_t num_list_len) {
+    
+    int* answer = (int*)malloc(1);
+    for (int i = 0; i < num_list_len; i++)
+    {
+        answer[i] = num_list[num_list_len - 1 - i];
+    }
+
+    return answer;
+}
