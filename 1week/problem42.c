2a70f16f5c1d24db74bf05cf5e26197cd487b9c7
ttae-young <happysally05@naver.com>
2024-10-05 21:42:34 +0900

    Solve 프로그래머스 문제 '합성수 찾기'

---
+++ 1week/problem42.c
@@ -0,0 +1,28 @@
+// # 합성수 찾기
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) 
+{
+    int answer = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int a = 0;
+
+        for (int j = 1; j <= i; j++)
+        {
+            if ((i % j) == 0)
+            {
+                a++;
+            }
+        }
+        
+        if (a >= 3)
+        {
+            answer++;
+        }
+    }
+    return answer;
+}
