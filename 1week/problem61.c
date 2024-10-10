e80123577a67caf8026b914f15fd93af9e8a12f1
ttae-young <happysally05@naver.com>
2024-10-10 15:51:33 +0900

    Solve 프로그래머스 문제 '편지'

---
+++ 1week/problem61.c
@@ -0,0 +1,14 @@
+// # ÆíÁö
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int solution(const char* message)
+{
+    int answer = 2 * strlen(message);
+    
+    return answer;
+}
