8c9dbfaf40f567f4b0cd3e9f4d516b6ada229d5c
ttae-young <happysally05@naver.com>
2024-10-11 21:57:58 +0900

    Solve 프로그래머스 문제 'OX퀴즈'

---
+++ 1week/problem68.c
@@ -0,0 +1,44 @@
+// # OXÄûÁî
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char** solution(const char* quiz[], size_t quiz_len) 
+{
+    char** answer = (char**)malloc(sizeof(char*)*quiz_len);
+    
+    for (size_t i = 0; i < quiz_len; i++)
+    {
+        int x, y, z;
+        char oper, equal;
+        sscanf(quiz[i], "%d %c %d %c %d", &x, &oper, &y, &equal, &z);
+        
+        int result = 0;
+
+        if (oper == '+')
+        {
+            result = x + y;
+        }
+        else if (oper == '-')
+        {
+            result = x - y;
+        }
+
+        if (result == z)
+        {
+            answer[i] = (char*)malloc(2);
+            answer[i] = "O";
+        }
+        else
+        {
+            answer[i] = (char*)malloc(2);
+            answer[i] = "X";
+        }
+
+    }
+
+    return answer;
+}
