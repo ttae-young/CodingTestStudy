9350e7bf94fb52ab329af574a5fa24cc2618d416
ttae-young <happysally05@naver.com>
2024-10-12 20:19:55 +0900

    Solve 프로그래머스 문제 '중복된 숫자 개수'

---
+++ 1week/problem75.c
@@ -0,0 +1,18 @@
+// # 중복된 숫자 개수
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int array[], size_t array_len, int n) 
+{
+    int answer = 0;
+
+    for (int i = 0; i < array_len; i++)
+    {
+        if (array[i] == n)
+            answer++;
+    }
+
+    return answer;
+}
