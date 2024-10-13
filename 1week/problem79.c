a5763af88f03dc13d3238594c24ee50b175d4ef5
ttae-young <happysally05@naver.com>
2024-10-13 22:20:03 +0900

    Solve 프로그래머스 문제 '최댓값 만들기 (2)'

---
+++ 1week/problem79.c
@@ -0,0 +1,24 @@
+// 최댓값 만들기 (2)
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+int solution(int numbers[], size_t numbers_len)
+{
+    int answer = -100000000;
+    int value = 0;
+
+    for (int i = 0; i < numbers_len-1; i++)
+    {
+        for (int j = i + 1; j < numbers_len;j++)
+        {
+            value = numbers[i] * numbers[j];
+            if (answer < value)
+                answer = value;
+        }
+    }
+
+    return answer;
+}
