fa1140863a5c39635c8edad98f82e9b866bcb52f
ttae-young <happysally05@naver.com>
2024-11-26 17:32:20 +0900

    Solve 백준 문제 '최대 힙'

---
+++ 4week/최대 힙.c
@@ -0,0 +1,64 @@
+// # 11279
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int heap[100001];
+int heap_size = 0;
+
+void insert(int value)
+{
+	int i = ++heap_size;
+	while (i != 1 && value > heap[i / 2])
+	{
+		heap[i] = heap[i / 2];
+		i /= 2;
+	}
+	heap[i] = value;
+}
+
+int delete_max()
+{
+	if (heap_size == 0)
+		return 0;
+
+	int max_value = heap[1];
+	int temp = heap[heap_size--];
+	int parent = 1, child;
+
+	while (parent * 2 <= heap_size)
+	{
+		child = parent * 2;
+		if (child < heap_size && heap[child] < heap[child + 1])
+			child++;
+		if (temp >= heap[child])
+			break;
+
+		heap[parent] = heap[child];
+		parent = child;
+	}
+	heap[parent] = temp;
+
+	return max_value;
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		scanf("%d", &x);
+
+		if (x == 0)
+		{
+			printf("%d\n", delete_max());
+		}
+		else
+		{
+			insert(x);
+		}
+	}
+	return 0;
+}
